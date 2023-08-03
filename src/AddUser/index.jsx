327201deec9d5afaf26c4055644b264309cea501
Chetan Rathore <chetanrathore@578@gmail.com>
2023-08-03 23:02:12 +0530

    initial commit

---
+++ src/AddUser/index.jsx
@@ -0,0 +1,6 @@
+
+function UserForm() {
+  <form>
+
+  </form>
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
+import { useState } from "react"
+
+import Button from '@mui/material/Button'
+import Modal from '@mui/material/Modal'
+import Box from '@mui/material/Box'
+
 function App() {
+  const [openModal, setModalOpen] = useState<boolean>(false)
+  
+  const handleClose = () => setModalOpen(false)
+  const handleAddUserClick = () => setModalOpen(true)
+
   return (
     <div>
-      <h1>Hello friend...</h1>
+      <Button onClick={handleAddUserClick} variant="contained">Add User</Button>
+        <Modal
+          open={openModal}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+        <Box
+          sx={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+          }}
+        >
+          {/* <AddUserForm/> */}
+        </Box>
+      </Modal>
     </div>
   );
 }
